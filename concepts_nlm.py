8d23628ae7fa217aed261335953091b8c6cdba84
Subham Sharma <subham.iitm.ds@gmail.com>
2025-04-04 14:10:48 +0530

    Initial commit of SuperDost API

---
+++ concepts_nlm.py
@@ -0,0 +1,68 @@
+NLM_CONCEPT_TREE = {
+    "Introduction to forces": [
+        "Fundamental forces",
+        "Normal, Tension and friction"
+    ],
+    "Newton's law of motion": [
+        "First law (Law of inertia)",
+        "Momentum and its significance",
+        "Second law",
+        "Third law"
+    ],
+    "Equilibrium of forces": [
+        "Concurrent and Coplanar Forces",
+        "Equilibrium of forces"
+    ],
+    "Frame of reference": [
+        "Inertial and non-inertial F.O.R.",
+        "Pseudo Forces"
+    ],
+    "Free body diagram": [
+        "Fundamentals of drawing F.B.D.",
+        "Train problems(Tension)",
+        "Lift Problems(Normal)",
+        "Wedge problems(Resolving Forces)"
+    ],
+    "Constraint motion": [
+        "String and pulley constraint(Tension)",
+        "Wedge constraint(Normal)",
+        "Rigid Body Constraint(Introduction)"
+    ],
+    "Pulley problems": [
+        "Simple pulley system",
+        "Complex Pulley system",
+        "Mechanical advantage"
+    ],
+    "Spring": [
+        "Spring constant",
+        "Combination of Spring(parallel and series)",
+        "Spring pulley system",
+        "Equivalent Spring constant",
+        "Cutting of spring and string."
+    ],
+    "Pseudo force": [
+        "Problems involving pseudo force"
+    ],
+    "Friction": [
+        "Types of Friction",
+        "Direction of static and kinetic friction",
+        "Coefficient of friction",
+        "Limiting value of friction",
+        "Angle of friction",
+        "Transition of friction from static to kinetic",
+        "Contact Force",
+        "Angle of repose",
+        "Two block problem",
+        "Three or more block problem"
+    ],
+    "Angle of repose": [
+        "Motion on incline with angle more than angle of repose",
+        "Motion on incline with less than the angle of repose",
+        "Motion on vertical surface"
+    ],
+    "Multiple block system": [
+        "Concept of driving force",
+        "Method to find which surface slips",
+        "Method to find acceleration of blocks"
+    ]
+}
